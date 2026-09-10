d25d2ee3d26ec17c45b5ad5912d4ecb57d45ad62
Vcorp-com <mylaveeraraghavendra@gmail.com>
2026-09-10 23:44:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-   allowedDevOrigins: ['192.168.56.1']
+   /*allowedDevOrigins: ['192.168.56.1']*/
+  output: "export",
 };
 
 export default nextConfig;
